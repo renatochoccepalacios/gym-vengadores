83133168da24b83dc2a9d554d6420f71b249c675
renatochocce <renatochocce758@gmail.com>
2024-06-12 17:56:25 -0600

    hkhlyhl

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ buttonHamburguer.addEventListener('click', () => {
     if(navMenu.classList.contains('menu-close')) {
         navMenu.classList.add("menu-open");
         navMenu.classList.remove("menu-close");
+        iconBars.style.color = '#fff';
         iconBars.setAttribute('class', 'fa-solid fa-x') // reemplazar clase
     }else {
         navMenu.classList.remove("menu-open");
         navMenu.classList.add("menu-close");
+        iconBars.style.color = '#000';
         iconBars.setAttribute('class', 'fa-solid fa-bars') // reemplazar clase
 
     }
